518e13be40e8c1a44c86d524874149c7d68ebf44
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-11-07 22:00:13 +0000

    Console.logs out the user's input

--- index.js
+++ index.js
@@ -2,11 +2,54 @@
 
 let score = 25;
 
-let words = ["un chat", "un chien", "un lapin", "un oiseau", "une livre", "une pomme", "une assiette", "une tasse", "un frere", "une mere", "un mere", "une soeur", "un lit", "une toilette", 
+let words = ["un chat", "un chien", "un lapin", "un oiseau", "une livre", "une pomme", "une assiette", "une tasse", "un frere", "une mere", "un pere", "une soeur", "un lit", "une toilette", 
 "un crayon", "une fleur", "un train", "une voiture", "un velo", "un avion", "un ours en peluche"];
 
 let randomWord = words[Math.floor(Math.random() * words.length)];
 console.log(randomWord)
+
+let french_english = [ 
+    {"un chat": "cat"}, 
+    {"un chien": "dog"},
+    {"un lapin": "rabbit"}, 
+    {"un oiseau": "bird"}, 
+    {"une livre": "book"}, 
+    {"une pomme": "apple"}, 
+    {"une assiette": "plate"}, 
+    {"une tasse": "cup"}, 
+    {"un frere": "brother"}, 
+    {"un pere": "father"}, 
+    {"une mere": "mother"},  
+    {"une soeur": "sister"}, 
+    {"un lit": "bed"}, 
+    {"une toilette": "toilet"}, 
+    {"un crayon": "pencil"}, 
+    {"une fleur": "flower"},
+    {"un train": "train"}, 
+    {"une voiture": "car"}, 
+    {"un velo": "bike"}, 
+    {"un avion": "plane"}, 
+    {"un ours en peluche": "teddy"} 
+]
+
+document.querySelector('.word').textContent = randomWord;
+
+
+document.querySelector('.checkanswer').addEventListener('click', function () {
+    const guess = document.querySelector('.guess').value;
+    console.log(guess);
+});
+
+document.querySelector('.checkanswer').addEventListener('click', function() {
+    
+    for (let i = 0; i < french_english.length; i++) {
+        if(french_english[i] === document.querySelector('.guess').value) {
+            document.querySelector('.message').textContent = "Correct Answer";
+            console.log(document.querySelector('.guess').target.value);
+        }
+    }
+})
+
 
 switch (randomWord) {
     case "un chat":
@@ -59,14 +102,5 @@ switch (randomWord) {
       english = "teddy bear";
   }
 
-document.querySelector('.word').textContent = randomWord;
 
-document.querySelector('.checkanswer').addEventListener('click', function() {
-    console.log('I was clicked!')
-    document.querySelector('.test').textContent = "The Check Answer button was clicked"
-})
-
-document.querySelector('.checkanswer').addEventListener('click', function() {
-    document.querySelector()
-})
 
