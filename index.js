861ec8c4717bfe869fd4cb0ee6e30f69423ced63
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-11-07 22:22:38 +0000

    Adds event listener for the next button. Removes unnecessary switch statement

--- index.js
+++ index.js
@@ -35,72 +35,32 @@ let french_english = [
 document.querySelector('.word').textContent = randomWord;
 
 
-document.querySelector('.checkanswer').addEventListener('click', function () {
-    const guess = document.querySelector('.guess').value;
-    console.log(guess);
-});
+//document.querySelector('.checkanswer').addEventListener('click', function () {
+  //  const guess = document.querySelector('.guess').value;
+    //console.log(guess);
+//});
+
+document.querySelector('.score').textContent = score;
 
 document.querySelector('.checkanswer').addEventListener('click', function() {
+    const guess = document.querySelector('.guess').value;
+    console.log(guess);
     
     for (let i = 0; i < french_english.length; i++) {
-        if(french_english[i] === document.querySelector('.guess').value) {
-            document.querySelector('.message').textContent = "Correct Answer";
-            console.log(document.querySelector('.guess').target.value);
+        console.log(french_english[i]);
+       if(french_english[i].key === guess.value) {
+           document.querySelector('.message').textContent = "Correct Answer";
+           score += 1;
+        } else {
+            document.querySelector('.message').textContent = "Wrong Answer"
+            score -= 1;
         }
     }
 })
 
 
-switch (randomWord) {
-    case "un chat":
-      english = "cat";
-      break;
-    case "un chien":
-      english = "dog";
-      break;
-    case "un lapin":
-       english = "rabbit";
-      break;
-    case "un oiseau":
-      english = "bird";
-      break;
-    case "une livre":
-      english = "book";
-      break;
-    case "une pomme":
-      english = "apple";
-      break;
-    case "une assiette":
-      english = "plate";
-    case "une tasse":
-      english = "cup";
-    case "un frere":
-      english = "brother";
-    case "une mere":
-      english = "mother";
-    case "un pere":
-      english = "father";
-    case "une soeur":
-      english = "sister";
-    case "un lit":
-      english = "bed";
-    case "toilette":
-      english = "toilet";
-    case "un crayon":
-      english = "pencil";
-    case "une fleur":
-      english = "flower";
-    case "un train":
-      english = "train";
-    case "une voiture":
-      english = "car";
-    case "un velo":
-      english = "bike";
-    case "un avion":
-      english = "plane";
-    case "un ours en peluche":
-      english = "teddy bear";
-  }
-
+document.querySelector('.next').addEventListener('click', function() {
+    console.log('Next')
+});
 
 
